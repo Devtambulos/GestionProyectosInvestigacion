7c607ee600ee6dd0a983a21eceaf356848d4637d
Faber Hoyos <faberhoyos01@gmail.com>
2021-11-21 17:04:59 -0500

    sidebar cambiado por <aside>

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const SidebarLinks = () => {
   return (
-    <ul className='mt-12'>
-      <SidebarRoute to='' title='Inicio' icon='fas fa-home' />
-      <SidebarRoute to='/page2' title='Pagina2' icon='fas fa-smile-wink' />
-      <SidebarRoute to='/category1' title='Catego 1' icon='fab fa-amazon' />
-      <SidebarRoute to='/category1/page1' title='Test' icon='fas fa-car' />
+    <ul className="mt-12">
+      <SidebarRoute to="" title="Inicio" icon="fas fa-home" />
+      <SidebarRoute to="/page2" title="Pagina2" icon="fas fa-smile-wink" />
+      <SidebarRoute to="/category1" title="Catego 1" icon="fab fa-amazon" />
+      <SidebarRoute to="/category1/page1" title="Test" icon="fas fa-car" />
     </ul>
   );
 };
 
 const Logo = () => {
   return (
-    <div className='py-3 w-full flex flex-col items-center justify-center'>
-      <img src='logo.png' alt='Logo' className='h-16' />
-      <span className='my-2 text-xl font-bold text-center'>Título de Mi Aplicación</span>
+    <div className="py-3 w-full flex flex-col items-center justify-center">
+      <img src="logo.png" alt="Logo" className="h-16" />
+      <span className="my-2 text-xl font-bold text-center">
+        Título de Mi Aplicación
+      </span>
     </div>
   );
 };
@@ -24,22 +26,27 @@ const Logo = () => {
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
   return (
-    <div className='flex flex-col md:flex-row flex-no-wrap md:h-full'>
-      {/* Sidebar starts */}
+    <aside>
+      <div className="flex flex-col md:flex-row flex-no-wrap md:h-full">
+        {/* Sidebar starts */}
 
-      <div className='sidebar hidden md:flex'>
-        <div className='px-8'>
-          <Logo />
-          <SidebarLinks />
+        <div className="sidebar hidden md:flex">
+          <div className="px-8">
+            <Logo />
+            <SidebarLinks />
+          </div>
         </div>
+        <div className="flex md:hidden w-full justify-between bg-gray-800 p-2 text-white">
+          <i
+            className={`fas fa-${open ? "times" : "bars"}`}
+            onClick={() => setOpen(!open)}
+          />
+          <i className="fas fa-home" />
+        </div>
+        {open && <ResponsiveSidebar />}
+        {/* Sidebar ends */}
       </div>
-      <div className='flex md:hidden w-full justify-between bg-gray-800 p-2 text-white'>
-        <i className={`fas fa-${open ? 'times' : 'bars'}`} onClick={() => setOpen(!open)} />
-        <i className='fas fa-home' />
-      </div>
-      {open && <ResponsiveSidebar />}
-      {/* Sidebar ends */}
-    </div>
+    </aside>
   );
 };
 
@@ -47,10 +54,10 @@ const ResponsiveSidebar = () => {
   return (
     <div>
       <div
-        className='sidebar h-full z-40 absolute md:h-full sm:hidden transition duration-150 ease-in-out'
-        id='mobile-nav'
+        className="sidebar h-full z-40 absolute md:h-full sm:hidden transition duration-150 ease-in-out"
+        id="mobile-nav"
       >
-        <div className='px-8'>
+        <div className="px-8">
           <Logo />
           <SidebarLinks />
         </div>
@@ -66,13 +73,13 @@ const SidebarRoute = ({ to, title, icon }) => {
         to={to}
         className={({ isActive }) =>
           isActive
-            ? 'sidebar-route text-white bg-indigo-700'
-            : 'sidebar-route text-gray-900 hover:text-white hover:bg-indigo-400'
+            ? "sidebar-route text-white bg-indigo-700"
+            : "sidebar-route text-gray-900 hover:text-white hover:bg-indigo-400"
         }
       >
-        <div className='flex items-center'>
+        <div className="flex items-center">
           <i className={icon} />
-          <span className='text-sm  ml-2'>{title}</span>
+          <span className="text-sm  ml-2">{title}</span>
         </div>
       </NavLink>
     </li>
