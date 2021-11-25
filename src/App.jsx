8877dcc60d02a0b8e0ab7e078212164fab5ddd5e
mreyesq <marcereyesq@gmail.com>
2021-11-24 20:23:45 -0500

    Tabla de usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,34 @@ import Page2 from 'pages/Page2';
 import IndexCategory1 from 'pages/category1/Index';
 import Category1 from 'pages/category1/CategoryPage1';
 import 'styles/globals.css';
-import Usuario from 'pages/usuario/Index';
-import Registro from 'pages/auth/registro';
-import AuthLayout from 'layouts/AuthLayout';
+import UsuarioIndex from 'pages/usuarios';
+import EditarUsuario from 'pages/usuarios/editar';
+import 'styles/globals.css';
+import 'styles/tabla.css';
+//import Registro from 'pages/auth/registro';
+//import AuthLayout from 'layouts/AuthLayout';
 import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-// const httpLink = createHttpLink({
-//   uri: 'https://gestion-proyectos-dev.herokuapp.com/graphql',
-// });
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = JSON.parse(localStorage.getItem('token'));
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
 function App() {
@@ -30,14 +47,12 @@ function App() {
           <Routes>
             <Route path='/' element={<PrivateLayout />}>
               <Route path='' element={<Index />} />
-              <Route path='usuario' element={<Usuario />} />
+              <Route path='usuarios' element={<UsuarioIndex />} />
+              <Route path='usuarios/editar' element={<EditarUsuario />} />
               <Route path='page2' element={<Page2 />} />
               <Route path='category1' element={<IndexCategory1 />} />
               <Route path='category1/page1' element={<Category1 />} />
             </Route>
-             <Route path='/auth' element={<AuthLayout />}>
-                <Route path='registro' element={<Registro />} />
-              </Route>
           </Routes>
         </BrowserRouter>
       </UserContext.Provider>
---
+++ src/pages/usuarios/editar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const EditarUsuario = () => {
+  return <div>EditarUsuario</div>;
+};
+
+export default EditarUsuario;
+
