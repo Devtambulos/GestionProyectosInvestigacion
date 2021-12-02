da3c64e6fc299620c6e0fe86a3b050048bc7e884
Faber Hoyos <faberhoyos01@gmail.com>
2021-12-02 16:37:29 -0500

    cambios en la carpeta pages/proyecto: se cambió parte en el index.jsx, se agregó Proyecto.jsx y ProyectoNuevo.jsx.
    Se editó el componente Sidebar.jsx.
    
    Se agregó carpeta en src, con nombre assets, donde se agregaron carpetas:
    
    img: donde se guardaron alguno iconos usados para el Card de proyectos.
    vectors: donde se guardó el vector de agregar proyecto (vector = icono).
    
    Se agregó en carpeta components un nuevo archivo con nombre Card.jsx:
    
    En este se hizo el diseño con uso de props para mostrar una parte de la información de los proyectos.
    Tambien se encuentra la función que muestra la card de un nuevo proyecto.
    
    Se editó la el archivo global.css en la carpeta styles:
    
    se editó para agregar estilos en los heading (h1, h2, h3, h4), la clase hide no se usa y tampoco myDIV pero fueron agregadas.
    
    En el archivo App.jsx se editó:
    
    En este se agrregaron las rutas para un proyecto en especifico, 'proyectos/:_id' y para agregar un proyecto 'proyectos/crear'.
    
    Se editó el archivo tailwind.config.js donde se agregó un estilo particular para las cards donde se muestran los proyectos

--- src/pages/proyecto/Index.jsx
+++ src/pages/proyecto/Index.jsx
@@ -1,90 +1,58 @@
-import React, {useEffect} from 'react';
-import { useQuery } from '@apollo/client';
-import { toast } from 'react-toastify';
-import { Link } from 'react-router-dom';
-import 'styles/tabla.css';
-import PrivateRoute from 'components/PrivateRoute';
-import ReactLoading from 'react-loading';
-import { GET_PROYECTOS } from 'graphql/proyectos/queries';
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
+import "styles/tabla.css";
+import PrivateRoute from "components/PrivateRoute";
+import ReactLoading from "react-loading";
+import { GET_PROYECTOS } from "graphql/proyectos/queries";
+import Card from "../../components/Card";
+import { CardNew } from "../../components/Card";
+import PrivateComponent from "../../components/PrivateComponent";
 
 const IndexProyecto = () => {
   const { data, error, loading } = useQuery(GET_PROYECTOS);
 
   useEffect(() => {
-    console.log('data servidor', data);
+    console.log("data servidor uno", data);
   }, [data]);
 
   useEffect(() => {
-      if (error) {
-        toast.error('Error consultando los usuarios');
-      }
-    }, [error]);
+    if (error) {
+      toast.error("Error consultando los usuarios");
+    }
+  }, [error]);
 
-//para ver la ruedita mientras carga la info de usuarios
-if (loading) return <div className="flex justify-center items-center w-full h-full"><ReactLoading type='spin'color='blue' height={'20%'} width={'20%'} /> </div>;
-
-
+  //para ver la ruedita mientras carga la info de usuarios
+  if (loading)
+    return (
+      <div className="flex justify-center items-center w-full h-full">
+        <ReactLoading type="spin" color="blue" height={"20%"} width={"20%"} />{" "}
+      </div>
+    );
 
   return (
-    <PrivateRoute roleList={["LIDER","ADMINISTRADOR",]}>
+    <PrivateRoute roleList={["LIDER", "ADMINISTRADOR"]}>
       <div className="p-8 items-center font-serif text-gray-800">
         <div className="p-2 m-4 text-3xl font-serif text-gray-800 font-bold text-center w-full justify-center ">
           Proyectos
         </div>
-        <table className='tabla '>
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Presupuesto</th>
-              <th>Inicio</th>
-              <th>Fin</th>
-              <th>Estado</th>
-              <th>Fase</th>
-              <th>Lider</th>
-              <th>Objetivos</th>
-              <th>Avances</th>
-              <th>Editar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data &&
-              data.Proyectos.map((u) => {
-                return (
-                  <tr key={u._id}>
-                    <td>{u.nombre}</td>
-                    <td>{u.presupuesto}</td>
-                    <td>{u.fechaInicio}</td>
-                    <td>{u.fechaFin}</td>
-                    <td>{u.estado}</td>
-                    <td>{u.fase}</td>
-                    <td>
-                      <div> id: {u.lider._id}</div>
-                      <div>{u.lider.nombre}</div>
-                      <div>{u.lider.apellido}</div>
-                      <div>{u.lider.identificacion}</div>
-                    </td>
-                    <td>
-                      <div> id: {u.objetivos._id}</div>
-                      <div>{u.objetivos.descripcion}</div>
-                      <div>{u.objetivos.tipo}</div>
-                    </td>
-                    <td>
-                      <div>id: {u.avances._id}</div>
-                      <div>{u.avances.fecha}</div>
-                      <div>{u.avances.descripcion}</div>
-                      <div>{u.avances.observaciones}</div>
-                    </td>
-                    <td>
-                      <Link to={`/usuarios/editar/${u._id}`}>
-                        <i className='fas fa-pen text-green-400 hover:text-green-600 cursor-pointer
-                        p-2 hover:bg-green-100 rounded-full' />
-                      </Link>
-                    </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
+        <div className="px-8 py-8 sm:px-16 grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5">
+          <PrivateComponent roleList={["ADMINISTRADOR", "LIDER"]}>
+            <CardNew />
+          </PrivateComponent>
+          {data.Proyectos.map((proyecto) => {
+            return (
+              <Card
+                Key={proyecto._id}
+                nombre={proyecto.nombre}
+                lider={proyecto.lider.nombre}
+                estado={proyecto.estado}
+                fase={proyecto.fase}
+              />
+            );
+          })}
+        </div>
       </div>
     </PrivateRoute>
   );
