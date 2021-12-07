e9c60c692f052baa6b9da8c6bb3f01796f1f31e8
JuanPabloRP <jpabloroldanp@gmail.com>
2021-12-06 19:09:44 -0500

    pagina inicial lista excepto por el main que tiene texto de prueba, es responsive

--- src/pages/inicial/index.jsx
+++ src/pages/inicial/index.jsx
@@ -1,31 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/inicio.css"
-import imgHeader from"../../images/undraw_project_completed_re_pqqq.svg"
+import imgHeader from"../../images/2.svg"
 
 
 const PaginaInicial = () => {
-  return <div>
+  return <div className="pI">
     <NavBar />
     <Header />
     <Main />
+    <Footer />
   </div>;
 };
 
-const NavBar = () => {
-  return(
-    <div className="bg-indigo-300 flex flex-wrap w-full containerNavBar">
-      <div>
-        <Logo />
-      </div>
-      <div>
-        <Redirigir irA={"/auth/register"} text={"Registro"}/>
-        <Redirigir irA={"/auth/login"} text={"Login"}/>
-      </div>
-    </div>
-  
-  )
-}
 
 const Logo = () => {
   return (
@@ -38,26 +25,42 @@ const Logo = () => {
 
 const Redirigir = ({irA, text,}) => {
   return (
-    <button className={`mx-5 sm:mx-0 md:m-5 `}>
-      <Link className={`px-3 py-2 bg-indigo-400 rounded-md text-white hover:bg-indigo-700 hover:text-white font-semibold  `} to={irA}>{text}</Link>
+    <button className={`mx-5 sm:mx-0 md:m-5 sm:px-3`}>
+      <Link className={`px-3 py-2 bg-indigo-900 rounded-md text-white hover:bg-indigo-700 hover:text-white font-semibold  `} to={irA}>{text}</Link>
     </button>
   );
 };
 
 
+const NavBar = () => {
+  return(
+    <div className="bg-indigo-300 flex flex-wrap w-full px-5 items-center sticky top-0 border-b border-solid border-black justify-center md:justify-between ">
+      <div className="px-7 md:p-0">
+        <Logo />
+      </div>
+      <div className="px-7 md:p-0">
+        <Redirigir irA={"/auth/register"} text={"Registro"}/>
+        <Redirigir irA={"/auth/login"} text={"Login"}/>
+      </div>
+    </div>
+  
+  )
+}
+
+
 const Header = () => {
   return(
-    <div className="bg-indigo-300 headerInicio">
-      <header className="headerMain">
-        <div className="containerTitulo flex justify-center">
-          <h1 className="font-bold text-5xl titulo textTitulo ">DevProject es el lugar indicado donde podras gestionar tus proyectos</h1>
-          <p className="textTitulo">¡Facil y gratis!</p>
+    <div className="bg-indigo-300 border-b border-solid border-black ">
+      <header className="grid grid-cols-1 md:grid-cols-2">
+        <div className="col-span-1 flex flex-wrap justify-center items-center p-5 md:row-span-1 ">
+          <h1 className="font-bold text-5xl p-1 w-full text-center ">DevProject es el lugar indicado donde podras gestionar tus proyectos</h1>
+          <p className="text-2xl  textTitulo w-full text-center p-5">¡Facil y gratis!</p>
 
           <Redirigir irA={"/auth/register"} text={"Empezar"}/>
           
         </div>
-        <image className="containerImgHeader">
-          <img src={imgHeader} alt="" />
+        <image className="md:col-span-1 p-5">
+          <img className="" src={imgHeader} alt="imgHeader" />
         </image>
 
       </header>
@@ -69,17 +72,39 @@ const Header = () => {
 const Main = () => {
   return(
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius aspernatur unde autem, odio reiciendis fugiat in adipisci? A nam atque necessitatibus dolorem odit neque cum modi? Doloremque, aperiam unde.
-      Aliquid debitis nostrum inventore fuga exercitationem aliquam, quam deserunt in recusandae accusantium esse quod delectus rerum ipsam! Ipsam aliquam velit consectetur, explicabo rem molestiae delectus vitae in suscipit! Vero, eos.
-      Voluptatem ex inventore atque. Omnis blanditiis repellendus nisi quos perspiciatis adipisci eos libero est praesentium? Mollitia quae repellendus rerum explicabo, accusamus provident sit delectus optio ea porro, odit voluptatem dolorem.
-      Officiis, id ullam? Esse obcaecati voluptates assumenda molestias magni doloremque voluptatem animi? Deleniti dolor aperiam labore, rerum ipsum assumenda quos possimus rem laborum dicta eligendi alias odio enim nisi nostrum.
-      Provident explicabo, labore inventore ex temporibus, repudiandae cum voluptas neque soluta repellendus nostrum accusamus expedita hic minima perferendis, laudantium odio sed? Eos sed saepe corrupti dicta repellat animi perspiciatis hic.
-      Numquam nulla inventore dignissimos. Ullam excepturi doloremque, natus, blanditiis impedit animi velit dolore tenetur delectus, omnis architecto odit ipsa nisi ad soluta ducimus incidunt harum? Necessitatibus laudantium praesentium doloremque provident?
-      Et enim placeat nam tempora incidunt, nemo quas nulla blanditiis, dolorem rerum ad suscipit neque possimus ab nisi! Sequi possimus dolorem iusto, dolorum alias natus voluptatibus? Hic temporibus assumenda optio.
-      Cumque ullam aliquid, beatae recusandae iste placeat quia, autem officia unde ipsa quod, numquam culpa corrupti? Natus at iusto iste rem molestias hic. Fugit suscipit rerum facilis sed quam et.
-      Illum velit eos, vitae quis dolor odit! Harum libero molestias error quibusdam reiciendis mollitia inventore rem in impedit, earum dolore laudantium voluptatem nemo, illum asperiores repellendus atque nam perspiciatis eum.
-      Cupiditate ullam provident iure illum consequuntur voluptatem veritatis assumenda? Magni quibusdam aspernatur eos aperiam! Laborum eaque aliquid non soluta nulla blanditiis at magnam omnis itaque, tenetur vitae ullam vel! Inventore?
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, omnis a. Porro, laborum molestias facere praesentium dolorem ducimus quia iusto! Id adipisci iste vero tempora rerum libero, maiores praesentium repudiandae.
+      Sit quidem quas magnam nostrum? Nihil ex quam sequi soluta debitis magnam adipisci at ducimus qui nisi. Eos, repudiandae, commodi qui rem autem dicta laboriosam facilis voluptas et ducimus beatae.
+      Rerum beatae accusantium exercitationem itaque provident veritatis veniam laboriosam ad numquam magnam! Sint doloribus numquam magni incidunt. Id tempora ipsa earum unde facere. Veniam reprehenderit iste quasi? Modi, harum voluptatibus?
+      Ab laboriosam, delectus magni reprehenderit ipsum ex nemo deleniti corrupti voluptatibus pariatur reiciendis laudantium doloremque quas vero et esse incidunt perspiciatis. Eum labore accusantium minus et laborum dolorem aut deserunt?
+      Libero, veritatis assumenda. Sit earum saepe, minus nisi ut nam et velit soluta quasi repellendus asperiores itaque reiciendis officiis cumque eveniet reprehenderit, sunt odio assumenda molestiae praesentium dolore a. Reprehenderit.
+      Illum esse sapiente vero amet ex, error debitis aperiam, at eos, quos nobis corrupti quia dolorum. Ipsum qui dolorem illum, repellendus reprehenderit, perspiciatis quo ullam, quia ex enim eligendi odio?
+      Odit soluta temporibus expedita debitis rem, mollitia corporis cumque! Qui id error voluptas necessitatibus molestias rem. Officia aliquid nihil neque, sequi quam, eos non laudantium asperiores aperiam molestiae amet adipisci.
+      Aut odit recusandae ut dolorum sed fugit dolore eos nesciunt placeat quas! Assumenda nisi sunt, provident eligendi at aliquam dolorem, esse natus nesciunt, repellendus soluta? Rem tenetur recusandae molestiae beatae?
+      Odit enim praesentium deleniti cum corporis mollitia neque eius maiores eos sapiente odio sunt ullam ipsum provident nostrum magni veniam error fuga quam iure, minus quibusdam sequi! Corporis, eveniet maiores?
+      Voluptas dolore, deserunt consectetur eligendi explicabo quas, provident aut qui repudiandae consequatur nobis ab nostrum odit incidunt saepe deleniti voluptate animi tempora veniam quibusdam. Quas quam ullam pariatur? Asperiores, beatae.
+      Voluptate fugiat quae hic adipisci, aperiam ipsa eaque nemo quas quod aut molestias dolorum, consequatur rerum vero autem nam, tempora vel at atque quo eligendi sapiente. Enim totam id sunt?
+      Sapiente, exercitationem necessitatibus corporis similique totam iste cum architecto sed hic vitae unde maiores, veniam placeat mollitia asperiores aspernatur consectetur pariatur accusamus perferendis? Expedita similique voluptatibus ipsam autem quibusdam quo.
+      Quisquam fugit dignissimos accusamus labore voluptatem excepturi quis distinctio voluptatibus nulla similique. Alias blanditiis cum atque aliquam, deleniti fugit distinctio, quas repudiandae aperiam ducimus illo repellendus dignissimos suscipit eveniet laboriosam!
+      Nostrum incidunt nesciunt ipsa? Recusandae laboriosam itaque libero at? Ea facilis veritatis harum cumque sequi velit dolor quia optio hic minus impedit enim, saepe itaque dolore facere? Natus, facere ducimus.
+      Est, dolorem inventore vero quaerat facere alias dolorum. Delectus cupiditate mollitia asperiores vero quas ullam repellendus similique soluta, doloremque odio aliquid, in aspernatur iure ratione doloribus aliquam blanditiis. Beatae, quis!
     </div>
   );
 }
+
+const Footer = () => {
+  return(
+    <div className="bg-indigo-300 flex flex-wrap w-full px-8 py-4 items-center justify-center sm:justify-between">
+      <p className="px-5 md:p-0">Hecho con <i className="fas fa-heart"/> y <i className="fa fa-battery-full"/> desde Colombia</p>
+      <a className="px-5 md:p-0" href="https://github.com/Devtambulos" rel="noreferrer" target="_blank">
+        <div className="border-b border-solid border-indigo-900 hover:border-indigo-100 ">
+          <span className="text-indigo-900 hover:text-indigo-100"> DevTambulos <i className="fa fa-github "/> </span>
+          
+        </div>
+      </a>
+    </div>
+  );
+}
+
+
+
 export default PaginaInicial;
