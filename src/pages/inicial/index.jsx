432c43d6a12394e23324212c46cceabfd54a414f
JuanPabloRP <jpabloroldanp@gmail.com>
2021-12-14 21:30:07 -0500

    factorizado con prettier xd

--- src/pages/inicial/index.jsx
+++ src/pages/inicial/index.jsx
@@ -96,7 +96,10 @@ const Section = () => {
     <>
       {datosInfoEmpresa.map((section) => {
         return (
-          <section className=" grid grid-cols-1 md:grid-cols-2 py-2 p-6 m-4 justify-center items-center w-9/12 border-4 border-gray-500 border-double estilosSection" key={section.titulo}>
+          <section
+            className=" grid grid-cols-1 md:grid-cols-2 py-2 p-6 m-4 justify-center items-center w-9/12 border-4 border-gray-500 border-double estilosSection"
+            key={section.titulo}
+          >
             {section.textFirst ? (
               <>
                 <div>
