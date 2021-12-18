0db64ffd17e50284325480a73d2b512d3ae3977a
JuanPabloRP <jpabloroldanp@gmail.com>
2021-12-17 22:22:14 -0500

    añadido más información en inicio privado

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import '../styles/inicio.css';
+
+import { info } from '.././utils/inicioPrivado/datosCard.js';
 
 const Index = () => {
   return (
-    <div>
-      <div className='bg-white h-96'>Página principal</div>
+    <div className=" w-full h-full">
+      <h1 className="text-center text-5xl font-bold p-5 text-indigo-700">
+        ¡Bienvenid@ a DevProject!
+      </h1>
+      <h3 className="text-center">
+        Algunas cosas que te gustará saber de DevProject:
+      </h3>
+      <CardInfo />
     </div>
+  );
+};
 
+// TODO: terminar cards de mostrar info xd
 
+const CardInfo = () => {
+  return (
+    <div className="flex flex-wrap justify-center">
+      {info.map((e) => {
+        return (
+          <div className="w-80 p-5 m-4 bg-white cardInicioPrivado">
+            <h4 className="text-center text-2xl p-5">{e.titulo}</h4>
+            <p className="text-xl ">{e.parrafo}</p>
+            
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
